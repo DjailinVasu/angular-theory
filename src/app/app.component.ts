8a3fe9d0de5855533286799e7b40986bef8cd2cc
Mary Denysova <djailin.vasu@gmail.com>
2020-04-08 20:36:55 +0300

    Передача параметров

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular components';
+
+  posts: Post[] = [
+    {
+      title: 'Я пробую изучать компоненты Angular',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, nemo!',
+      id: 1
+    },
+    {
+      title: 'Надеюсь, я не трачу время в пустую',
+      text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis illum accusamus consectetur enim nobis neque!',
+      id: 2
+    }
+  ]
 }
