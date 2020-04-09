d747cfd8535bb35913921bd63e028339eac7e442
Mary Denysova <djailin.vasu@gmail.com>
2020-04-09 15:40:47 +0300

    События и параметры в директиве

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,12 +1,26 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
 
+  @Input('appStyle') color: string = 'pink'
+  @Input() backgroundColor: string = '#ddd'
   constructor(private el: ElementRef, private r: Renderer2) {
-      this.r.setStyle(el.nativeElement, 'border-color', 'pink')
   }
 
+  @HostListener('click', ['$event']) onClick(event: Event) {
+      console.log(event);
+  }
+
+  @HostListener('mouseenter') onEnter() {
+      this.r.setStyle(this.el.nativeElement, 'border-color', this.color);
+      this.r.setStyle(this.el.nativeElement, 'background-color', this.backgroundColor)
+  }
+
+  @HostListener('mouseleave') onLeave() {
+    this.r.setStyle(this.el.nativeElement, 'border-color', null);
+    this.r.setStyle(this.el.nativeElement, 'background-color', null)
+  }
 }
