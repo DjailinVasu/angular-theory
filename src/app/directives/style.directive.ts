580fd3a6a08e27181fc3767db2b79bc6228e263f
Mary Denysova <djailin.vasu@gmail.com>
2020-04-09 15:45:00 +0300

    Работа директивы с HostBinding

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, Input, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
@@ -7,8 +7,12 @@ export class StyleDirective {
 
   @Input('appStyle') color: string = 'pink'
   @Input() backgroundColor: string = '#ddd'
+  @Input() borderRadius: string = '0px'
+
   constructor(private el: ElementRef, private r: Renderer2) {
   }
+
+  @HostBinding('style.borderRadius') elBorderRadius = null
 
   @HostListener('click', ['$event']) onClick(event: Event) {
       console.log(event);
@@ -16,11 +20,15 @@ export class StyleDirective {
 
   @HostListener('mouseenter') onEnter() {
       this.r.setStyle(this.el.nativeElement, 'border-color', this.color);
-      this.r.setStyle(this.el.nativeElement, 'background-color', this.backgroundColor)
+      this.r.setStyle(this.el.nativeElement, 'background-color', this.backgroundColor);
+
+      this.elBorderRadius = this.borderRadius;
   }
 
   @HostListener('mouseleave') onLeave() {
     this.r.setStyle(this.el.nativeElement, 'border-color', null);
-    this.r.setStyle(this.el.nativeElement, 'background-color', null)
+    this.r.setStyle(this.el.nativeElement, 'background-color', null);
+
+    this.elBorderRadius = null;
   }
 }
